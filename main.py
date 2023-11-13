a8fe79abb760744def4c0d53cb683d1e7ca366fc
FWork <fahengraphic@gmail.com>
2023-11-13 18:16:38 -0300

    Snake in OOP

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,16 +8,7 @@ screen.bgcolor("midnightblue")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-starting_position = [(0,0), (-20,0),(-40,0)]
-
-segments = []
-
-for position in starting_position:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
 
 game_is_on = True
 
@@ -25,11 +17,7 @@ while game_is_on:
     time.sleep(0.1)
     #for seg in segments:
     #    seg.forward(20)
-    for seg_num in range(len(segments)-1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move()
 
 
 screen.exitonclick()
