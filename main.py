47a545c756ece6e5071518e9a0c7bf1ba7f74002
Fahen_Yoda <fahendev@gmail.com>
2023-11-13 08:04:06 -0300

    Create starting positions (posições iniciais)

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ screen.setup(width=600, height=600)
 screen.bgcolor("midnightblue")
 screen.title("My Snake Game")
 
+starting_position = [(0,0), (-20,0),(-40,0)]
+
+for position in starting_position:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
+
 screen.exitonclick()
